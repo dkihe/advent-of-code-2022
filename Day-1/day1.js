a2dd686ac42607a60ee03343ec90a88e6dc09122
Damien Kihe <dnkkihe@gmail.com>
2022-12-02 23:07:23 -1000

    Completed Day 1

---
+++ Day-1/day1.js
@@ -0,0 +1,45 @@
+// --- Day 1: Calorie Counting ---
+// https://adventofcode.com/2022/day/1
+
+// Answers for problem
+let ans1, ans2
+
+// Array used to hold total calories
+const elfCalories = []
+
+// Require File System Module
+let fs = require('fs')
+// Array containing input.txt
+const inputArr = fs.readFileSync('./input.txt').toString().split("\n")
+
+// Add calories and push each total amount to elfCalories array
+const addCalories = arr => {
+    let total = 0 
+    for (i in arr) {
+        if (arr[i] !== "") {
+            total = total + parseInt(arr[i]) 
+        }
+        else {
+            elfCalories.push(total)
+            total = 0
+        }
+    }
+}
+
+// Fill elfCalories Array
+addCalories(inputArr)
+
+// Output (Part 1): 68467
+ans1 = Math.max(...elfCalories)
+
+// Sort Array by Descending order
+const sortedArr = elfCalories.sort((a,b) => b - a)
+
+// Top 3 elves
+const topElves = []
+topElves.push(sortedArr[0])
+topElves.push(sortedArr[1])
+topElves.push(sortedArr[2])
+
+// Output (Part 2): 203420
+ans2 = topElves.reduce((a, b) => a + b)
