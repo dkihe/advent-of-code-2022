6a1c9582a3a3b07318b1976074eb3f718252a737
Damien Kihe <dnkkihe@gmail.com>
2022-12-13 20:31:46 -1000

    Day 8 checkpoint

--- Day-8/day8.js
+++ Day-8/day8.js
@@ -4,13 +4,14 @@
 // Require File System Module 
 let fs = require('fs')
 // Array containing input.txt
-let data = fs.readFileSync('./input.txt').toString().trim().split('\n')
+let data = fs.readFileSync('./test.txt').toString().trim().split('\n')
 
 let total = 0
 let grid = []
 
 // Create a 2d array
 data.map(x => grid.push(x))
+grid = grid.map(x => x.trim())
 
 for (let col = 0; col <= grid[0].length; col++) {
     for (let row = 0; row <= grid.length; row++) {
@@ -19,4 +20,17 @@ for (let col = 0; col <= grid[0].length; col++) {
         }
     }
 }
-// console.log(grid[2][18])
+
+let checkVisible = (row, col) => {
+    let tree = grid[row][col]
+    // Check left adn right
+    let left = grid[row].slice(0,col)
+    let right = grid[row].slice(col+1, grid[row].length)
+    if (tree > Math.max(...left) || tree > Math.max(...right)) {
+        console.log(true)
+        total++
+    }
+}
+
+console.log(grid[3][3])
+checkVisible(3,3)
